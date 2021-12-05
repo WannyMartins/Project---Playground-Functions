a73568ed4a0c7e062c5100f02f7b213b6a85b91e
Wanny Martins <wcatery@gmail.com>
2021-12-05 14:35:21 -0300

    iniciando o 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,9 +15,43 @@
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+
+  let iRepete=[];
+  let parenteses = ["("+ array[0] + array[1] +")"];
+  let primeirosNumeros = [" "+array[2] + array[3] + array[4] + array[5]+ array[6]];
+  let ultimosNumeros = ["-" + array[7] + array[8] + array[9] + array[10]];
+  let telefoneCompleto = parenteses + primeirosNumeros + ultimosNumeros;
+
+    for(i of array){
+      i===array[i];
+      iRepete.push(array[i]);
+      if(iRepete.length>3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    };
+  
+    for(let x of array){
+      if(x<0 || x> 9 ){
+      return "não é possível gerar um número de telefone    com esses valores";
+      } 
+    };
+  
+
+
+    for(let n of array){
+    
+      if (array.length < 11 && array.length > 11){
+        return "Array com tamanho incorreto.";
+
+      } else {
+        return telefoneCompleto;
+
+      }
+    };
+
 }
+
 
 // Desafio 12
 function triangleCheck() {
