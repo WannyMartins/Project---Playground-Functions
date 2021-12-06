6c810ae06095f7903e5bb6c3f5b1f2c7f089941d
Wanny Martins <wcatery@gmail.com>
2021-12-06 15:33:55 -0300

    tentando resolver o 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,40 +17,62 @@
 // Desafio 11
 function generatePhoneNumber(array) {
 
-  let iRepete=[];
-  let parenteses = ["("+ array[0] + array[1] +")"];
-  let primeirosNumeros = [" "+array[2] + array[3] + array[4] + array[5]+ array[6]];
-  let ultimosNumeros = ["-" + array[7] + array[8] + array[9] + array[10]];
-  let telefoneCompleto = parenteses + primeirosNumeros + ultimosNumeros;
 
-    for(i of array){
-      i===array[i];
-      iRepete.push(array[i]);
-      if(iRepete.length>3){
-        return "não é possível gerar um número de telefone com esses valores";
+  function arrayEhValido(array){
+    if(array.length !== 11){
+      return false;
+    }
+    return true;
+  }
+
+>>>>>> function repeteMaisDe3(array){
+    let numeroAtual=0;
+    let repetidoQuantasVezes=0;
+
+    for(let i= 0; i<array.length; i +=1){
+      if(array[i]=== numeroAtual){
+      repetidoQuantasVezes+=1;
       }
-    };
-  
-    for(let x of array){
-      if(x<0 || x> 9 ){
-      return "não é possível gerar um número de telefone    com esses valores";
-      } 
-    };
-  
+    }
+    if (repetidoQuantasVezes > 3) {
+      return true;
+    }
+  }
 
+  function numerosValidos(array){
+    for(let numero of array){
+      if(numero <  0 || numero > 9){
+        return false;
+      }
+      return true
+    }
+  }
 
-    for(let n of array){
+  function telefoneCompleto(array){
+    let parenteses = "("+ array[0] + array[1] +")";
+    let primeirosNumeros = " "+array[2] + array[3] + array[4] + array[5]+ array[6];
+    let ultimosNumeros = "-" + array[7] + array[8] + array[9] + array[10];
+
+      let telefone = parenteses + primeirosNumeros + ultimosNumeros;
+      return telefone
+
+  }
+
     
-      if (array.length < 11 && array.length > 11){
-        return "Array com tamanho incorreto.";
 
-      } else {
-        return telefoneCompleto;
-
-      }
-    };
-
+/*   function validando(){
+    if(numerosValidos === true || repeteMaisDe3 === true){
+      return "não é possível gerar um número de telefone com esses valores"
+    }else if(arrayEhValido === true){
+      return"Array com tamanho incorreto."
+    }else{
+      return telefoneCompleto
+    }
+    return validando
+  }
+ */
 }
+
 
 
 // Desafio 12
