d76c52750bc7c99bcfb64f55c15797d8171ca0ce
Wanny Martins <wcatery@gmail.com>
2022-02-06 14:29:12 -0300

    finalizando todo o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -10,52 +10,52 @@ function compareTrue(p1, p2) {
 
 // Desafio 2
 function calcArea(base, heigth) {
-  let area=(base*heigth)/2;
+  let area = (base * heigth) / 2;
   return area
 
 }
 
 // Desafio 3
-function splitSentence(string){
+function splitSentence(string) {
 
-let split= string.split(" ")
+  let split = string.split(" ")
 
 
-return split
+  return split
 }
- 
+
 
 // Desafio 4
 function concatName(param) {
   let resultado;
 
-    
-    resultado=param[param.length-1] +", "+param[0];
 
-  
+  resultado = param[param.length - 1] + ", " + param[0];
+
+
   return resultado
 
 
 }
 // Desafio 5
 function footballPoints(wins, ties) {
- let vitorias = wins*3;
- let result= vitorias+ties; 
- return result
+  let vitorias = wins * 3;
+  let result = vitorias + ties;
+  return result
 }
 
 // Desafio 6
 function highestCount(numeros) {
   let numeroMaior = numeros[0];
   let quantidade = 0;
-  for(let item in numeros){
-    if(numeros[item] > numeroMaior){
+  for (let item in numeros) {
+    if (numeros[item] > numeroMaior) {
       numeroMaior = numeros[item];
     }
   };
-  for(let i in numeros){
-    if(numeros[i] == numeroMaior){
-      quantidade+=1;
+  for (let i in numeros) {
+    if (numeros[i] == numeroMaior) {
+      quantidade += 1;
     };
   }
   return quantidade
@@ -69,11 +69,11 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = Math.abs(mouse - cat1);
   let distanciaCat2 = Math.abs(mouse - cat2);
 
-  if (distanciaCat1 < distanciaCat2){
+  if (distanciaCat1 < distanciaCat2) {
     return 'cat1'
-    }else if (distanciaCat1 > distanciaCat2){
+  } else if (distanciaCat1 > distanciaCat2) {
     return 'cat2'
-  } else if(distanciaCat1 == distanciaCat2) {
+  } else if (distanciaCat1 == distanciaCat2) {
     return "os gatos trombam e o rato foge"
   };
 }
@@ -83,27 +83,27 @@ function catAndMouse(mouse, cat1, cat2) {
 //referencia do for/of tirada do stackoverflow, onde neste caso organizou a varredura no array pelo valor dele: https://pt.stackoverflow.com/questions/90352/qual-%c3%a9-a-diferen%c3%a7a-entre-o-for-of-e-o-for-in
 function fizzBuzz(numbers) {
 
- let resultado = [];
-  for(let num of numbers) {
-    if(num %3 == 0 && num %5 == 0) {
-     resultado.push('fizzBuzz')
-    } else if (num %5 == 0) {
+  let resultado = [];
+  for (let num of numbers) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      resultado.push('fizzBuzz')
+    } else if (num % 5 == 0) {
       resultado.push('buzz')
-    } else if (num %3 == 0) {
+    } else if (num % 3 == 0) {
       resultado.push('fizz')
     } else {
       resultado.push('bug!');
     }
   }
-return resultado;
+  return resultado;
 }
 
 // Desafio 9
 function encode(string) {
 
-   let stringEnc='';
+  let stringEnc = '';
 
-  let codigos= {
+  let codigos = {
     a: 1,
     e: 2,
     i: 3,
@@ -111,24 +111,24 @@ function encode(string) {
     u: 5
   };
 
-  for(let letra of string){
-   stringEnc= stringEnc + (codigos[letra] || letra)
+  for (let letra of string) {
+    stringEnc = stringEnc + (codigos[letra] || letra)
   };
 
   return stringEnc;
 }
- 
+
 function decode(string) {
-  let stringDec ='';
-  let code= {
-    1:"a",
-    2:"e",
-    3:"i",
-    4:"o",
-    5:"u"
+  let stringDec = '';
+  let code = {
+    1: "a",
+    2: "e",
+    3: "i",
+    4: "o",
+    5: "u"
   };
-  for(let num of string){
-   stringDec= stringDec + (code[num] || num)
+  for (let num of string) {
+    stringDec = stringDec + (code[num] || num)
   };
   return stringDec;
 }
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,11 @@
 // Desafio 10
- function techList(tec, name) {
-  lista = [];
-  if (tec.length == 0) {
+function techList(tec, name) {
+  const lista = [];
+  if (tec.length === 0) {
     return 'Vazio!';
   }
-  tec.sort();//cria lista ordenada.
-  for (var key in tec){
+  tec.sort();// cria lista ordenada.
+  for (let key in tec) {
     lista.push({
       tech: tec[key],
       name: name
@@ -14,75 +14,81 @@
   return lista;
 }
 
-// Desafio 11
-function generatePhoneNumber(array) {
 
 
-  function arrayEhValido(array){
-    if(array.length !== 11){
-      return false;
-    }
-    return true;
-  }
+function telefoneCompleto(array) {
+  let parenteses = "(" + array[0] + array[1] + ")";
+  let primeirosNumeros = " " + array[2] + array[3] + array[4] + array[5] + array[6];
+  let ultimosNumeros = "-" + array[7] + array[8] + array[9] + array[10];
 
->>>>>> function repeteMaisDe3(array){
-    let numeroAtual=0;
-    let repetidoQuantasVezes=0;
+  let telefone = parenteses + primeirosNumeros + ultimosNumeros;
+  return telefone
 
-    for(let i= 0; i<array.length; i +=1){
-      if(array[i]=== numeroAtual){
-      repetidoQuantasVezes+=1;
-      }
-    }
-    if (repetidoQuantasVezes > 3) {
-      return true;
-    }
-  }
-
-  function numerosValidos(array){
-    for(let numero of array){
-      if(numero <  0 || numero > 9){
-        return false;
-      }
-      return true
-    }
-  }
-
-  function telefoneCompleto(array){
-    let parenteses = "("+ array[0] + array[1] +")";
-    let primeirosNumeros = " "+array[2] + array[3] + array[4] + array[5]+ array[6];
-    let ultimosNumeros = "-" + array[7] + array[8] + array[9] + array[10];
-
-      let telefone = parenteses + primeirosNumeros + ultimosNumeros;
-      return telefone
-
-  }
-
-    
-
-/*   function validando(){
-    if(numerosValidos === true || repeteMaisDe3 === true){
-      return "não é possível gerar um número de telefone com esses valores"
-    }else if(arrayEhValido === true){
-      return"Array com tamanho incorreto."
-    }else{
-      return telefoneCompleto
-    }
-    return validando
-  }
- */
 }
 
 
+// Desafio 11
+function generatePhoneNumber(array) {
+  let telefone = '';
+  let contador = 0;
+
+    if (array.length !== 11) {
+      return "Array com tamanho incorreto."
+    }
+    for (let i = 0; i < array.length; i += 1){
+      if (array[i] < 0 || array[i] > 9){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+      for (let j = 1; j < array.length; j += 1){
+        if (array[i] === array[j]){
+          contador += 1;
+        }
+      }
+      if (contador >= 3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+      contador = 0
+    }
+  
+    let parenteses = "(" + array[0] + array[1] + ")";
+    let primeirosNumeros = " " + array[2] + array[3] + array[4] + array[5] + array[6];
+    let ultimosNumeros = "-" + array[7] + array[8] + array[9] + array[10];
+  
+   telefone = parenteses + primeirosNumeros + ultimosNumeros;
+    return telefone
+  
+
+  }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (c < (a + b) && 
+    b < (a + c) && 
+    a < (c + b) &&
+    c > Math.abs(a - b) && 
+    b > Math.abs(a - c) && 
+    a > Math.abs(c - b)) {
+      return true;
+    }
+    return false;
+  // Math.abs retorna o valor absoluto sendo negativo retorna o numero real -1 >>> 1,  1 >> 1, '-1' >>> 1, 
+  // null ou vazio >>0, array com mai de um numero, objeto vazio, 
+  // string que nao é um numero, e ele próprio vazio retorna NaN
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let coposAgua=0
+  let novaString = string.replace(/[^0-9]/g,'') // começa a catalogar todos o primeiro numero de 0-9 'g' de forma global sem parar na primeira incidencia, o segundo paragamento mostra ṕra replace substituir tudo o que não for do paramentro anterior por nada.
+  for(let number = 0; number < novaString.length; number += 1) {
+   coposAgua += parseInt(novaString[number]);// transforma os itens numeros da string em numeros interos podendo ser somado
+  }
+  return coposAgua > 1 ? coposAgua + ' copos de água' : coposAgua + ' copo de água'
+ 
+
+  // fonte do replace()>> https://pt.stackoverflow.com/questions/3719/como-obter-apenas-os-n%C3%BAmeros-de-uma-string-em-javascript
+
+  // fonte do regex https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp
 }
 
 module.exports = {
